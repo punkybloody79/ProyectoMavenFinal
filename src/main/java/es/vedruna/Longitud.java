1c78471a24f54f1b0691d3f28fafce825c682947
ElenaSegun <helenidad@gmail.com>
2025-06-05 20:13:13 +0200

    creacion de programa

---
+++ src/main/java/es/vedruna/Longitud.java
@@ -0,0 +1,22 @@
+package es.vedruna;
+
+public class Longitud {
+
+    public int getWordLength(String word){
+        if (validador(word)) {
+            return word.length();
+        }
+        // Si la palabra no pasa el validador devuelve 0
+        return 0;
+    }
+
+
+    public boolean validador(String word) {
+        if (word== null) {
+            return false; // Manejo de caso nulo
+        }
+        return true;
+
+
+    }
+}
--- src/main/java/es/vedruna/Main.java
+++ src/main/java/es/vedruna/Main.java
@@ -2,6 +2,6 @@ package es.vedruna;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
